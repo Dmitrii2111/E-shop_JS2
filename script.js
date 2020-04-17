0f8e2b9c152131435fd921584fdb61e65faecb1c
d.hrulev <dmitryhrulev@gmail.com>
2020-04-17 14:09:32 +0300

    замена For for на Each

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ const goods = [
   { title: "Underwear", price: 150, src: "img/underwear.png" },
 ];
 
-const renderGoodsItem = (title  = 'name', price = 'price', src ='Pic') => {
+const renderGoodsItem = (title = "name", price = "price") => {
   return `<div class="goods_item"><h3>${title}</h3><p class='wear_price'>${price}</p><button class="goods_btn add_btn">Добавить</button><button class="goods_btn del_btn">Удалить</button></div>`;
 };
 
 const renderGoodsList = (list) => {
   let goodsList = list.map((item) =>
-    renderGoodsItem(item.title, item.price , item.src)
+    renderGoodsItem(item.title, item.price)
   );
-  document.querySelector(".goods_list").innerHTML = goodsList.join(" ");
-
+  document.querySelector(".goods_list").innerHTML = goodsList.join(" "); // join решил проблему с запятой
 };
 
 renderGoodsList(goods);
 
-let goodsItem = document.querySelectorAll('.wear_price'); // Добавлене контейнера с изображением
-for(let i =0; i < goods.length; i++) {
-  goodsItem[i].insertAdjacentHTML('beforebegin','<div class="image_container"></div>');
-};
+let goodsItem = document.querySelectorAll(".wear_price"); // Добавлене контейнера с изображением
+goodsItem.forEach((element) => {
+  element.insertAdjacentHTML(
+    "beforebegin",
+    '<div class="image_container"></div>'
+  );
+});
 
-let imageContainer = document.querySelectorAll('.image_container'); // Добавлене  изображения в контейнер 
-for(let i =0; i < goods.length; i++) {
+let imageContainer = document.querySelectorAll(".image_container"); // Добавлене  изображения в контейнер
+for (let i = 0; i < goods.length; i++) {
   imageContainer[i].style.backgroundImage = `url(${goods[i].src})`;
-};
-
-
+}
 
